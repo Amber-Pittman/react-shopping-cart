fa8c1b5be91ecbea9cffe867fcb319b5dd9b66d3
Amber-Pittman <amber@amberpittman.com>
2019-10-16 20:19:12 -0400

    Completed Step 4: Consuming Data with ProductContext

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,25 @@
-import React from 'react';
+// import the useContext Hook here
+import React, { useContext } from 'react';
 
 // Components
 import Product from './Product';
 
+// import Context here
+import { ProductContext } from "../contexts/ProductContext";
+
+
 const Products = props => {
+	// Call the useContext Hook
+	// Destructure products and addItem
+	const { products, addItem } = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
