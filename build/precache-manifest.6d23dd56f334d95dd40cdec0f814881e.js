d163c934f46c29477445d2cc7825fc25513d4087
Amber-Pittman <amber@amberpittman.com>
2022-04-29 21:47:17 -0400

    Created build file for Netlify

---
+++ build/precache-manifest.6d23dd56f334d95dd40cdec0f814881e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d425f17862a8fece406e33475b7b7eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "127bb3039fc9276b0510",
+    "url": "/static/css/main.94b0b514.chunk.css"
+  },
+  {
+    "revision": "54b079d97cdff99eafa9",
+    "url": "/static/js/2.56a88161.chunk.js"
+  },
+  {
+    "revision": "127bb3039fc9276b0510",
+    "url": "/static/js/main.970cc414.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
